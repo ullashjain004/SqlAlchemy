f86a52c7a4d9e90f6c7c36403c20f8dc6547b9f9
Sneha Philip <snehaphilip989@gmail.com>
2019-11-27 12:43:01 -0500

    Feature: Updated test file with customer object

--- Database/sqlalite_test.py
+++ Database/sqlalite_test.py
@@ -2,9 +2,29 @@ from sqlalchemy.orm import create_engine
 # create a Session object to use Session class from sqlalchemy.orm
 from sqlalchemy.orm import sessionmaker
 
-# Creating connection between alchemy and sql lite 
+# Creating connection between alchemy and sql lite
 engine = create_engine('sqlite:////web/Sqlite-Data/example4.db')
 Session = sessionmaker(bind=engine)
 
 session = Session()
+
+# Inserting data
+# Creating new customer objects
+
+c1 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
+
+c2 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
+c1, c2
 
