aed3d63786ae2bdb1296cb1da17c38723ea4eed7
Sneha Philip <snehaphilip989@gmail.com>
2019-11-27 12:34:30 -0500

    Feature: Created connection between alchemy and sql alite

--- Database/sqlalite_test.py
+++ Database/sqlalite_test.py
@@ -2,8 +2,9 @@ from sqlalchemy.orm import create_engine
 # create a Session object to use Session class from sqlalchemy.orm
 from sqlalchemy.orm import sessionmaker
 
-
+# Creating connection between alchemy and sql lite 
 engine = create_engine('sqlite:////web/Sqlite-Data/example4.db')
 Session = sessionmaker(bind=engine)
 
 session = Session()
+
